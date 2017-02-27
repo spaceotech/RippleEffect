a6bc903f9cfda484bea15d21eaf9f0aa1c194c0c
hiteshtrivedi <hitesh_trivedi@spaceotechnologies.com>
2017-02-27 18:48:42 +0530

    Commit Changes

--- SORippleEffect/Model/RippleButton.swift
+++ SORippleEffect/Model/RippleButton.swift
@@ -18,19 +18,19 @@ class RippleButton: UIButton {
     }
     */
     
-    var circleShape = CALayer()
+    var animateLayer = CALayer()
     
     override func beginTrackingWithTouch(touch: UITouch, withEvent event: UIEvent?) -> Bool {
         let width: CGFloat = self.bounds.size.width
         let height: CGFloat = self.bounds.size.height
         if (self.imageView!.image != nil) {
-            self.circleShape = self.createImageShapeWithPosition(CGPointMake(width / 2, height / 2), pathRect: CGRectMake(-CGRectGetMidX(self.bounds), -CGRectGetMidY(self.bounds), self.imageView!.image!.size.width, self.imageView!.image!.size.height))
-            self.circleShape.contents = (self.imageView!.image!.CGImage! as AnyObject)
+            self.animateLayer = self.drawimgShape(CGPointMake(width / 2, height / 2), rect: CGRectMake(-CGRectGetMidX(self.bounds), -CGRectGetMidY(self.bounds), self.imageView!.image!.size.width, self.imageView!.image!.size.height))
+            self.animateLayer.contents = (self.imageView!.image!.CGImage! as AnyObject)
         }
         else {
-            self.circleShape = self.createCircleShapeWithPosition(CGPointMake(width / 2, height / 2), pathRect: CGRectMake(-CGRectGetMidX(self.bounds), -CGRectGetMidY(self.bounds), width, height), radius: self.layer.cornerRadius)
+            self.animateLayer = self.drawCircle(CGPointMake(width / 2, height / 2), rect: CGRectMake(-CGRectGetMidX(self.bounds), -CGRectGetMidY(self.bounds), width, height), radius: self.layer.cornerRadius)
         }
-        self.layer.addSublayer(self.circleShape)
+        self.layer.addSublayer(self.animateLayer)
         return true
     }
     
@@ -39,31 +39,31 @@ class RippleButton: UIButton {
     override func endTrackingWithTouch(touch: UITouch?, withEvent event: UIEvent?) {
         let point = touch!.locationInView(self)
         if point.x < 0 || point.y < 0 || point.x > self.bounds.size.width || point.y > self.bounds.size.height {
-            self.circleShape.removeFromSuperlayer()
+            self.animateLayer.removeFromSuperlayer()
         }
         else {
             let scale: CGFloat = 2.5
-            let groupAnimation = self.createFlashAnimationWithScale(scale, duration: 0.5)
+            let groupAnimation = self.animateWithFlash(scale, duration: 0.5)
             //
             //groupAnimation["circleShaperLayer"] = self!.circleShape
             groupAnimation.delegate = self
-            self.circleShape.addAnimation(groupAnimation, forKey: nil)
+            self.animateLayer.addAnimation(groupAnimation, forKey: nil)
         }
     }
     
     
-    func createImageShapeWithPosition(position: CGPoint, pathRect rect: CGRect) -> CALayer {
-        let imageLayer = CALayer()
-        imageLayer.bounds = rect
-        imageLayer.position = position
-        return imageLayer
+    func drawimgShape(pos: CGPoint, rect: CGRect) -> CALayer {
+        let imgLayer = CALayer()
+        imgLayer.bounds = rect
+        imgLayer.position = pos
+        return imgLayer
     }
     
     
-    func createCircleShapeWithPosition(position: CGPoint, pathRect rect: CGRect, radius: CGFloat) -> CAShapeLayer {
+    func drawCircle(pos: CGPoint, rect: CGRect, radius: CGFloat) -> CAShapeLayer {
         let circleShape = CAShapeLayer()
-        circleShape.path = self.createCirclePathWithRadius(rect, radius: radius)
-        circleShape.position = position
+        circleShape.path = self.drawCircleWithRadius(rect, radius: radius)
+        circleShape.position = pos
         circleShape.fillColor = UIColor.clearColor().CGColor
         circleShape.strokeColor = UIColor.purpleColor().CGColor
         circleShape.opacity = 0
@@ -72,7 +72,7 @@ class RippleButton: UIButton {
     }
     
     
-    func createFlashAnimationWithScale(scale: CGFloat, duration: CGFloat) -> CAAnimationGroup {
+    func animateWithFlash(scale: CGFloat, duration: CGFloat) -> CAAnimationGroup {
         let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
         scaleAnimation.fromValue = NSValue(CATransform3D: CATransform3DIdentity)
         scaleAnimation.toValue = NSValue(CATransform3D: CATransform3DMakeScale(scale, scale, 1))
@@ -86,7 +86,7 @@ class RippleButton: UIButton {
     }
     
     
-    func createCirclePathWithRadius(frame: CGRect, radius: CGFloat) -> CGPathRef {
+    func drawCircleWithRadius(frame: CGRect, radius: CGFloat) -> CGPathRef {
         return UIBezierPath(roundedRect: frame, cornerRadius: radius).CGPath
     }
     
